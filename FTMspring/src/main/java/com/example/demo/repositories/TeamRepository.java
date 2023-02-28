7fa7b448354a5d859198d0b4f2878416c0d70337
soham-ghatpande03 <ghatpande.soham03@gmail.com>
2023-02-28 12:35:10 +0530

    ftm removed from main

--- FTMspring/src/main/java/com/example/demo/repositories/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-	
-}
--- FTMspring/src/main/java/com/example/demo/repositories/TournamentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Tournament;
-
-public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
-
-}
