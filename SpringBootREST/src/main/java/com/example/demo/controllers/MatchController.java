270b008be0bf1a2b5f3f164fd88720e1f89eb306
Your Name <you@example.com>
2023-03-01 15:32:16 +0530

    new change

--- SpringBootREST/src/main/java/com/example/demo/controllers/MatchController.java
+++ SpringBootREST/src/main/java/com/example/demo/controllers/MatchController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.Match;
 import com.example.demo.services.MatchService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MatchController {
 	
