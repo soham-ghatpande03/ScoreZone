81e5a60f5de48a25a907e4e44510a7883847acf0
Parth <parthpatil.2000@gmail.com>
2023-03-02 23:29:05 +0530

    change

--- React_JS/myfirstreact/src/components/LoginCompCheck.js
+++ React_JS/myfirstreact/src/components/LoginCompCheck.js
@@ -1,113 +1,131 @@
-import { useReducer , useState} from "react";
+import { useReducer, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "./slice";
 
-export default function LoginComp () {
+export default function LoginComp() {
 
     const init = {
-        uid: {value:"",error:"",valid:false, touched:false},
-        pwd:{value:"",error:"",valid:false, touched:false}
+        uid: { value: "", error: "", valid: false, touched: false },
+        pwd: { value: "", error: "", valid: false, touched: false },
+        isFormValid: false
     }
 
     const reducer = (state, action) => {
-            switch(action.type)
-            {
-                case 'update':
-                     return {...state , [action.fld]: {  ...state[action.fld],value: action.val, error: action.error, valid: action.valid, touched: action.touched}}
-                case 'reset' :
-                     return init;
-            }
+        switch (action.type) {
+            case 'update':
+                return { ...state, [action.fld]: { ...state[action.fld], value: action.val, error: action.error, valid: action.valid, touched: action.touched }, isFormValid }
+            case 'reset':
+                return init;
+        }
     }
 
-    const [info, dispatch] = useReducer(reducer,init) ;
+    const [info, dispatch] = useReducer(reducer, init);
     const [msg, setMsg] = useState("");
     const navigate = useNavigate();
-    const reduxAction  = useDispatch();
+    const reduxAction = useDispatch();
 
-    const validate = (nm,val) => {
+    const validate = (nm, val) => {
         let error = "";
         let valid = false;
         let touched = true;
-        switch(nm)
-        {
-            case 'uid' :
-                 const exp1 = /[A-Za-z0-9._-]{5,12}/
-                 if(!exp1.test(val))
-                 {
-                    error = "Invalid UID";                    
-                 }
-                 else
-                 {
-                    error ="";
+        switch (nm) {
+            case 'uid':
+                const exp1 = /[A-Za-z0-9._-]{5,12}/
+                if (!exp1.test(val)) {
+                    error = "Invalid UID";
+                }
+                else {
+                    error = "";
                     valid = true;
-                 }
-                 break;
+                }
+                break;
 
             case 'pwd':
-                 break;     
+                const exp2 = /[A-Za-z0-9._-]{5,12}/
+                if (!exp2.test(val)) {
+                    error = "Invalid password";
+                }
+                else {
+                    error = "";
+                    valid = true;
+                }
+                break;
         }
-        console.log(val+","+error+","+valid)
-        dispatch({type: 'update', fld: nm,value: val,error, valid, touched})
+        console.log(val + "," + error + "," + valid)
+        //dispatch({ type: 'update', fld: nm, value: val, error, valid, touched })
+        return ({ valid, error });
     }
 
+    const PostValidData = () => {
+        const [state, dispatch] = useReducer(reducer, init)
+        const [msg, setMsg] = useState("");
+    }
+
+    const onInputChange = (name, value) => {
+        const { hasError, error } = validate(name, value);
+        let isFormValid = true;
+        for (const key in state) {
+            let item = state[key];
+            if (key === name && hasError) {
+                isFormValid = false;
+                break;
+            }
+
+            else if (key !== name && item.hasError) {
+                isFormValid = false;
+                break;
+            }
+        }
+    }
 
     const sendData = (e) => {
         e.preventDefault();
         const reqOptions = {
             method: 'POST',
-            headers: {'content-type':'application/json'},
+            headers: { 'content-type': 'application/json' },
             body: JSON.stringify(info)
         }
-        fetch("http://localhost:8080/chkLogin",reqOptions)
-        .then(resp =>  {if(resp.ok)
-                        {
-                            console.log(resp.status)
-                            return resp.text();
-                        }    
-                         else
-                         {
-                            console.log(resp.statusText)
-                             throw new Error("Server error"); 
-                         }
-                        })
-        .then(text => text.length ? JSON.parse(text) : {})
-        .then(obj =>  {
-                       if(Object.keys(obj).length === 0)
-                       {
-                            setMsg("Wrong UID/PWD");
-                       } 
-                       else
-                       {
-                           reduxAction(login())
-                           localStorage.setItem("loggedUser",JSON.stringify(obj));
-                           if(obj.status === false) 
-                           {
-                               alert("Request has not been approved");
-                           }
-                           else
-                           {
+        fetch("http://localhost:8080/chkLogin", reqOptions)
+            .then(resp => {
+                if (resp.ok) {
+                    console.log(resp.status)
+                    return resp.text();
+                }
+                else {
+                    console.log(resp.statusText)
+                    throw new Error("Server error");
+                }
+            })
+            .then(text => text.length ? JSON.parse(text) : {})
+            .then(obj => {
+                if (Object.keys(obj).length === 0) {
+                    setMsg("Wrong UID/PWD");
+                }
+                else {
+                    reduxAction(login())
+                    localStorage.setItem("loggedUser", JSON.stringify(obj));
+                    if (obj.status === false) {
+                        alert("Request has not been approved");
+                    }
+                    else {
 
-                               if(obj.role_id.role_id === 1)
-                               {
-                                    navigate("/admin_home");
-                               }
-                               else if(obj.role_id.role_id === 2)
-                               {
-                                    navigate("/doctor_home");
-                               }
-                               else if(obj.role_id.role_id === 3)
-                               {
+                        if (obj.role_id.role_id === 1) {
+                            navigate("/admin_home");
+                        }
+                        else if (obj.role_id.role_id === 2) {
+                            navigate("/doctor_home");
+                        }
+                        else if (obj.role_id.role_id === 3) {
 
-                               }
-                               else if(obj.role_id.role_id === 4)
-                               {
+                        }
+                        else if (obj.role_id.role_id === 4) {
 
-                               }
-                           }
-                       }    
-                })
-         .catch((error) => alert("Server error. Try after some time"));       
+                        }
+                    }
+                }
+            })
+            .catch((error) => alert("Server error. Try after some time"));
     }
 
 
@@ -117,33 +135,33 @@ export default function LoginComp () {
             <form>
                 <div className="mb-3">
                     <label htmlFor="uid" className="form-label">Enter uid : </label>
-                    <input type="text" 
-                    className="form-control" 
-                    id="uid" 
-                    name="uid" 
-                    value={info.uid.value}
-                    onChange={(e)=>{validate("uid", e.target.value)}}   />
-                    <div 
-                    id="emailHelp"
-                     className="form-text" 
-                     style={{display: (!info.uid.valid&&info.uid.touched)?"block":"none"}}>
-                    {info.uid.error}
+                    <input type="text"
+                        className="form-control"
+                        id="uid"
+                        name="uid"
+                        value={info.uid.value}
+                        onChange={(e) => { onInputChange("uid", e.target.value) }} />
+                    <div
+                        id="emailHelp"
+                        className="form-text"
+                        style={{ display: (!info.uid.valid && info.uid.touched) ? "block" : "none" }}>
+                        {info.uid.error}
                     </div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="pwd" className="form-label">Enter uid : </label>
-                    <input type="password" className="form-control" id="pwd" name="pwd"  value={info.pwd.value} 
-                    onChange={(e)=>{validate("pwd", e.target.value)}} />
+                    <input type="password" className="form-control" id="pwd" name="pwd" value={info.pwd.value}
+                        onChange={(e) => { onInputChange("pwd", e.target.value) }} />
                     <div id="emailHelp"
-                     className="form-text" 
-                     style={{display: !info.pwd.valid&&info.pwd.touched?"block":"none"}}> 
-                    {info.pwd.error}
+                        className="form-text"
+                        style={{ display: !info.pwd.valid && info.pwd.touched ? "block" : "none" }}>
+                        {info.pwd.error}
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary mb-3" onClick={(e)=> {sendData(e)}}>Submit</button>
-                <button type="reset" className="btn btn-primary mb-3" onClick={()=> {dispatch({type:'reset'})}} > Clear </button>
+                <button type="submit" className="btn btn-primary mb-3" onClick={(e) => { sendData(e) }}>Submit</button>
+                <button type="reset" className="btn btn-primary mb-3" onClick={() => { dispatch({ type: 'reset' }) }} > Clear </button>
 
-              
+
             </form>
 
             <p> {JSON.stringify(info)}</p>
--- React_JS/myfirstreact/src/components/UserRegister.js
+++ React_JS/myfirstreact/src/components/UserRegister.js
@@ -1,171 +1,171 @@
 import '../forms.css';
-import { useReducer} from "react";
+import { useReducer, useState } from "react";
 
 
-export default function Signup(){
+export default function Signup() {
 
-    const init = {
-        fname: "",
-        lname: "",
-        email: "",
-        contact: "",
-        username: "",
-        password: "",
-        type_id:"",
-        q_id: "",
-        q_answer: ""
+  const init = {
+    fname: "",
+    lname: "",
+    email: "",
+    contact: "",
+    username: "",
+    password: "",
+    type_id: "",
+    q_id: "",
+    q_answer: ""
+  }
+
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'update':
+        return { ...state, [action.fld]: action.val }
+      case 'reset':
+        return init;
     }
+  }
 
-    const reducer = (state, action) => {
-        switch(action.type)
-        {
-            case 'update' :
-                return {...state,[action.fld]:action.val}
-            case 'reset' :
-                return init;
-        }
+  const [info, dispatch] = useReducer(reducer, init)
+
+  const sendData = (e) => {
+    e.preventDefault();
+    const reqOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(info)
     }
+    fetch("http://localhost:8082/saveUser", reqOptions)
+      .then(resp => console.log(resp))
+  }
 
-    const [info, dispatch] = useReducer(reducer, init)
-
-    const sendData = (e) => {
-        e.preventDefault();
-        const reqOptions = {
-            method: 'POST',
-            headers: {'content-type':'application/json'},
-            body: JSON.stringify(info)
-        }
-        fetch("http://localhost:8082/saveUser", reqOptions)
-        .then(resp => console.log(resp))
-    }
-
-return(
+  return (
     <div className="auth-wrapper">
       <div className="auth-inner">
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <form>
-      <h3>Sign Up</h3>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <form>
+          <h3>Sign Up</h3>
 
-        <div className="mb-3">
-          <label>First Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter First Name"
-            id="First_Name"
-            name="First_Name"
-            value={info.fname}
-            onChange={(e) => {dispatch({type:'update', fld:'fname', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter First Name"
+              id="First_Name"
+              name="First_Name"
+              value={info.fname}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'fname', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Last Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Last Name"
-            id="Last_Name"
-            name="Last_Name"
-            value={info.lname}
-            onChange={(e) => {dispatch({type:'update', fld:'lname', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Last Name"
+              id="Last_Name"
+              name="Last_Name"
+              value={info.lname}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'lname', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Email</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Email"
-            id="Email"
-            name="Email"
-            value={info.email}
-            onChange={(e) => {dispatch({type:'update', fld:'email', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Email</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Email"
+              id="Email"
+              name="Email"
+              value={info.email}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'email', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Contact</label>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Enter Contact No"
-            id="Contact"
-            name="Contact"
-            value={info.contact}
-            onChange={(e) => {dispatch({type:'update', fld:'contact', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Contact</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter Contact No"
+              id="Contact"
+              name="Contact"
+              value={info.contact}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'contact', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Username"
-            id="Username"
-            name="Username"
-            value={info.username}
-            onChange={(e) => {dispatch({type:'update', fld:'username', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Username"
+              id="Username"
+              name="Username"
+              value={info.username}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'username', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter Password"
-            id="Password"
-            name="Password"
-            value={info.password}
-            onChange={(e) => {dispatch({type:'update', fld:'password', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter Password"
+              id="Password"
+              name="Password"
+              value={info.password}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'password', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>Security Question</label><br></br>
-          <select value={info.q_id}   onChange={(e) => {dispatch({type:'update', fld:'q_id', val: e.target.value})}}>
-            <option value={1} >Place</option>
-            <option value={2}>Color</option>
-            <option value={3}>Pet</option>
+          <div className="mb-3">
+            <label>Security Question</label><br></br>
+            <select value={info.q_id} onChange={(e) => { dispatch({ type: 'update', fld: 'q_id', val: e.target.value }) }}>
+              <option value={1} >Place</option>
+              <option value={2}>Color</option>
+              <option value={3}>Pet</option>
             </select>
-        </div>
+          </div>
 
-        <div className="mb-3">
-          <label>Security Answer</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter Answer"
-            id="Answer"
-            name="Answer"
-            value={info.qanswer}
-            onChange={(e) => {dispatch({type:'update', fld:'qanswer', val: e.target.value})}}
-          />
-        </div>
+          <div className="mb-3">
+            <label>Security Answer</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Answer"
+              id="Answer"
+              name="Answer"
+              value={info.qanswer}
+              onChange={(e) => { dispatch({ type: 'update', fld: 'qanswer', val: e.target.value }) }}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label>User type</label><br></br>
-          <select className="form-control" value={info.type_id}  onChange={(e) => {dispatch({type:'update', fld:'type_id', val:e.target.value})}}>
-          <option>Select User Type</option>
-            <option value={1}>Tournament Manager</option>
-            <option value={2}>Team Manager</option>
+          <div className="mb-3">
+            <label>User type</label><br></br>
+            <select className="form-control" value={info.type_id} onChange={(e) => { dispatch({ type: 'update', fld: 'type_id', val: e.target.value }) }}>
+              <option>Select User Type</option>
+              <option value={1}>Tournament Manager</option>
+              <option value={2}>Team Manager</option>
             </select>
-        </div>
+          </div>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary" onClick={(e) => {sendData(e)}}>
-            Submit
-          </button>
-        </div>
-      </form>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary" onClick={(e) => { sendData(e) }}>
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
       <p>{JSON.stringify(info)}</p>
-      </div>
+    </div>
 
-      
-    )
+
+  )
 
 }
