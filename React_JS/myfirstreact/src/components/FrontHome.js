3c10502e826925986d00b29a2a52db5f4cf4a302
soham-ghatpande03 <ghatpande.soham03@gmail.com>
2023-03-04 19:11:04 +0530

    new

--- React_JS/myfirstreact/src/components/FrontHome.js
+++ React_JS/myfirstreact/src/components/FrontHome.js
@@ -5,7 +5,7 @@ export default function FrontHome()
     return (
       
       <div >
-      <nav className="navbar navbar-expand-sm bg-light fixed-top ">
+      {/* <nav className="navbar navbar-expand-sm bg-light fixed-top ">
         <div className="container-fluid">
           <Link className="navbar-brand" to={'/'}>
             ScoreZone
@@ -33,15 +33,85 @@ export default function FrontHome()
           </div>
         </div>
 
-      </nav>
-      <section class="first">
+      </nav> */}
 
-<div class="container">
+<nav
+          className="navbar navbar-expand-lg bg-light fixed-top"
+          
+        >
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              VSMS
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contactus">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/servicecenters">
+                    Service Centers
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/userlogin">
+                    Login
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Register
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="/customerregistration">
+                        User
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="/servicecenterregistration"
+                      >
+                        Service Center
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-  
-</div>
 
-</section>
 <Outlet/>
       </div>
     )
