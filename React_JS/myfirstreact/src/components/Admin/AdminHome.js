a877b8a0cee3071b2ff81fdbe60f063b30c9222e
soham-ghatpande03 <ghatpande.soham03@gmail.com>
2023-03-10 01:36:35 +0530

    up to date

--- React_JS/myfirstreact/src/components/Admin/AdminHome.js
+++ React_JS/myfirstreact/src/components/Admin/AdminHome.js
@@ -1,5 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom'
-import { useReducer, useState, useEffect} from "react";
+import { useState, useEffect} from "react";
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 export default function AdminHome() {
 
   const [ad,setAd] = useState(null);
@@ -14,12 +20,12 @@ export default function AdminHome() {
    })
   } ,[])
 
-    return(
+    
 
-<div>
+{/* <div>
 <div>
             <h1>Welcome {ad && ad.first_name}</h1>
-        </div>
+        </div> 
 {/* <nav className="navbar navbar-expand-lg navbar-light  ">
           <div className="container">
             <Link class="font-weight-bold" className="navbar-brand" to={'/'}>
@@ -46,8 +52,8 @@ export default function AdminHome() {
               </ul>
             </div>
           </div>
-        </nav> */}
-<nav className="navbar navbar-expand-lg bg-dark fixed-top">
+        </nav> 
+{/* <nav className="navbar navbar-expand-lg bg-dark fixed-top">
         <div className="container-fluid">
           <a style={{color:'white'}} className="navbar-brand" href="/">
             <b>ScoreZone</b>
@@ -83,7 +89,60 @@ Welcome, {ad && ad.first_name}
       </nav> 
         
         <Outlet />
-        </div>
+        </div> */}
+
+        return(
+        <div >
+      <Navbar style={{fontSize:"25px", fontFamily:"Century Gothic"}}bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+      <Navbar.Brand href="/">
+            <img
+              src=""
+              width="350"
+              height="40"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            /> 
+          </Navbar.Brand>&nbsp
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/viewmatch"><b>APPROVE TOURNAMENT MANAGER</b></Nav.Link>&nbsp
+            <Nav.Link href="/allteams"><b>APPROVE TEAM MANAGER</b></Nav.Link>&nbsp
+            
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link> */}
+          </Nav>
+          <Nav.Link href="sign-in">
+            
+            <Button variant="outline-success">Login</Button>
+            
+            
+            </Nav.Link>
+            
+        
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+<Outlet/>
+
+      </div>
     )
 
 }
--- React_JS/myfirstreact/src/components/FrontHome.js
+++ React_JS/myfirstreact/src/components/FrontHome.js
@@ -1,7 +1,14 @@
 //import '../bgim.css';
 import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../index.css'
+import img1 from '../components/logo1.png'
 export default function FrontHome() 
 {
 
@@ -10,49 +17,49 @@ export default function FrontHome()
       
       <div >
         <div style={{display: mystate.loggedin ? "none" : "block"}} >
-<nav className="navbar navbar-expand-lg bg-dark fixed-top">
-          <div className="container-fluid">
-            <a style={{color:'white'}} className="navbar-brand" href="/">
-              <b>ScoreZone</b>
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a style={{color:'white'}} className="nav-link" href="viewmatch">
-                    <b>View Matches</b>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a style={{color:'white'}} className="nav-link " href="allteams">
-                  <b>View Teams</b>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a  style = {{textAlign:'right',position:'absolute',top:'10%',left:'93%'}}
-                   className="nav-link btn btn-light"  
-                   href="sign-in">
-                  <b>Login</b>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav> 
+      <Navbar style={{fontSize:"25px", fontFamily:"Century Gothic"}}bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+      <Navbar.Brand href="/">
+            <img
+              src={img1}
+              width="350"
+              height="40"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            /> 
+          </Navbar.Brand>&nbsp
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/viewmatch"><b>LIVE MATCHES</b></Nav.Link>&nbsp
+            <Nav.Link href="/allteams"><b>TEAMS</b></Nav.Link>&nbsp
+            <Nav.Link href="/"><b>PLAYERS</b></Nav.Link>
+            
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link> */}
+          </Nav>
+          <Nav.Link href="sign-in"><Button variant="outline-success">Login</Button></Nav.Link>
+            
         
-        </div>
-       
-
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+</div>
 <Outlet/>
 
       </div>
